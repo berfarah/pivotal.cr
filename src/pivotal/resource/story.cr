bb07eaa8a0bec611dfd9bc70a0279c8ff849093e
Bernardo Farah <ber@bernardo.me>
2016-11-06 20:32:31 -0800

    Changed Story to use Int32 because pivotal has huge numbers
    Tremendous numbers, people tell me

--- src/pivotal/resource/story.cr
+++ src/pivotal/resource/story.cr
@@ -21,17 +21,21 @@ module Pivotal
         Accepted
       end
 
-      def self.find(id : Int16, project_id)
+      def self.find(id : Int32?, project_id)
         response = Client.request(
           method: "GET", path: "projects/#{project_id}/stories/#{id}",
         )
         self.from_json(response.body)
       end
 
-      def self.all(project_id : Int16)
+      def self.all(project_id : Int32?)
+        return Array(self).new if project_id.nil?
+
         response = Client.request(
           method: "GET",
-          path: "/projects/#{project_id}/stories",)
+          path: "/projects/#{project_id}/stories"
+        )
+
         Array(self).from_json(response.body)
       end
 
@@ -49,7 +53,7 @@ module Pivotal
       end
 
       JSON.mapping(
-        id: {type: Int16 | Int32, default: -1},
+        id: {type: Int32, default: -1},
         name: {type: String, nilable: true},
         description: {type: String, nilable: true},
         story_type: {type: Type, default: Type::Feature},
@@ -58,7 +62,8 @@ module Pivotal
         accepted_at: {type: Time, converter: Converters::Time, nilable: true, emit_null: true},
         deadline: {type: Time, converter: Converters::Time, nilable: true},
         requested_by_id: {type: Int16 | Int32, nilable: true},
-        owner_ids: {type: Array(Int16 | Int32), default: Array(Int16 | Int32).new},
+        owner_ids: {type: Array(Int32), default: Array(Int32).new},
+        # TODO: Add labels here
         # label_ids: {type: Array(Int16 | Int32), default: Array(Int16 | Int32).new},
         created_at: {type: Time, converter: Converters::Time},
         updated_at: {type: Time, converter: Converters::Time},
