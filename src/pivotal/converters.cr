c4649c812cdfef9ca051b3c2a34aac2b780d73be
Bernardo Farah <ber@bernardo.me>
2016-11-11 23:37:10 -0500

    Actually namespaced Converters correctly

--- src/pivotal/converters.cr
+++ src/pivotal/converters.cr
@@ -1,22 +1,24 @@
-module Converters
-  ISO8601_TIME = "%Y-%m-%dT%TZ"
+module Pivotal
+  module Converters
+    ISO8601_TIME = "%Y-%m-%dT%TZ"
 
-  struct TimeFormat
-    def initialize(@time_format : String); end
+    struct TimeFormat
+      def initialize(@time_format : String); end
 
-    getter :time_format
+      getter :time_format
 
-    def from_json(pull : JSON::PullParser)
-      time_string = pull.read_string_or_null
-      return if time_string.nil?
-      ::Time.parse(time_string, time_format)
+      def from_json(pull : JSON::PullParser)
+        time_string = pull.read_string_or_null
+        return if time_string.nil?
+        ::Time.parse(time_string, time_format)
+      end
+
+      def to_json(value, io)
+        io << value.to_s(time_format)
+      end
     end
 
-    def to_json(value, io)
-      io << value.to_s(time_format)
-    end
+    Time = Converters::TimeFormat.new(ISO8601_TIME)
+    Date = Converters::TimeFormat.new("%F")
   end
-
-  Time = Converters::TimeFormat.new(ISO8601_TIME)
-  Date = Converters::TimeFormat.new("%F")
 end
